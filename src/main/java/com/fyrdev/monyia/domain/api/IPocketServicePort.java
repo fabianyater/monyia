2c81084636a816566ffb86e20a2613d0fa5b0868
Fabian Yate <fabianyate31@gmail.com>
2025-01-06 17:42:12 -0500

    add pocket interface ports

---
+++ src/main/java/com/fyrdev/monyia/domain/api/IPocketServicePort.java
@@ -0,0 +1,5 @@
+package com.fyrdev.monyia.domain.api;
+
+public interface IPocketServicePort {
+    void saveNewPocket();
+}
---
+++ src/main/java/com/fyrdev/monyia/domain/spi/IPocketPersistencePort.java
@@ -0,0 +1,5 @@
+package com.fyrdev.monyia.domain.spi;
+
+public interface IPocketPersistencePort {
+    void saveNewPocket();
+}
