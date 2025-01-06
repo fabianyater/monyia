c7f6ba6ec7d8d4dfad17c3ac7044f268a7770659
Fabian Yate <fabianyate31@gmail.com>
2025-01-06 18:10:05 -0500

    add pocket entity

---
+++ src/main/java/com/fyrdev/monyia/adapters/driven/jpa/entity/PocketEntity.java
@@ -0,0 +1,42 @@
+package com.fyrdev.monyia.adapters.driven.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "pocket")
+public class PocketEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private String uuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long balance;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_entity_id")
+    private UserEntity userEntity;
+
+}
