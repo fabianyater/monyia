4088d1b64c98f435f64ff4ea361cbd70a5214326
Fabian Yate <fabianyate31@gmail.com>
2025-04-11 22:58:48 -0500

    Enhance transaction ports by adding getTransactionSummaryByCategories method to retrieve transaction summaries by category

--- src/main/java/com/fyrdev/monyia/domain/api/ITransactionServicePort.java
+++ src/main/java/com/fyrdev/monyia/domain/api/ITransactionServicePort.java
@@ -1,12 +1,15 @@
 package com.fyrdev.monyia.domain.api;
 
 import com.fyrdev.monyia.domain.model.Transaction;
+import com.fyrdev.monyia.domain.model.TransactionSummaryByCategoriesResponse;
 import com.fyrdev.monyia.domain.model.enums.TransactionType;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface ITransactionServicePort {
     Transaction saveNewTransaction(Transaction transaction);
     BigDecimal getMonthlyIncome(Long pocketId);
     BigDecimal getMonthlyExpense(Long pocketId);
+    List<TransactionSummaryByCategoriesResponse> getTransactionSummaryByCategories(Long pocketId, TransactionType transactionType);
 }
--- src/main/java/com/fyrdev/monyia/domain/spi/ITransactionPersistencePort.java
+++ src/main/java/com/fyrdev/monyia/domain/spi/ITransactionPersistencePort.java
@@ -1,12 +1,15 @@
 package com.fyrdev.monyia.domain.spi;
 
 import com.fyrdev.monyia.domain.model.Transaction;
+import com.fyrdev.monyia.domain.model.TransactionSummaryByCategoriesResponse;
 import com.fyrdev.monyia.domain.model.enums.TransactionType;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface ITransactionPersistencePort {
     Transaction saveNewTransaction(Transaction transaction);
     BigDecimal getMonthlyIncome(Long pocketId, Long userId);
     BigDecimal getMonthlyExpense(Long pocketId, Long userId);
+    List<TransactionSummaryByCategoriesResponse> getTransactionSummaryByCategories(Long pocketId, Long userId, TransactionType transactionType);
 }
