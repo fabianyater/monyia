8cb824f31bcf874025f9efd3ea084b8e9a6b9512
Fabian Yate <fabianyate31@gmail.com>
2025-04-30 16:45:14 -0500

    Implement loan payment processing and update loan status upon completion

--- src/main/java/com/fyrdev/monyia/domain/api/usecase/LoanUseCase.java
+++ src/main/java/com/fyrdev/monyia/domain/api/usecase/LoanUseCase.java
@@ -1,11 +1,13 @@
 package com.fyrdev.monyia.domain.api.usecase;
 
+import com.fyrdev.monyia.configuration.exceptionhandler.ResourceNotFoundException;
 import com.fyrdev.monyia.domain.api.ILoanServicePort;
 import com.fyrdev.monyia.domain.exception.InsufficientBalanceException;
 import com.fyrdev.monyia.domain.model.Category;
 import com.fyrdev.monyia.domain.model.Loan;
 import com.fyrdev.monyia.domain.model.Pocket;
 import com.fyrdev.monyia.domain.model.Transaction;
+import com.fyrdev.monyia.domain.model.enums.LoanStatus;
 import com.fyrdev.monyia.domain.model.enums.LoanType;
 import com.fyrdev.monyia.domain.model.enums.Periodicity;
 import com.fyrdev.monyia.domain.model.enums.TransactionType;
@@ -52,6 +54,7 @@ public class LoanUseCase implements ILoanServicePort {
 
         loan.setStartDate(LocalDateTime.now());
         loan.setBalance(loanAmount);
+        loan.setLoanStatus(LoanStatus.ACTIVE);
 
         Loan savedLoan = loanPersistencePort.saveLoan(loan);
 
@@ -78,5 +81,52 @@ public class LoanUseCase implements ILoanServicePort {
     @Override
     public Loan getLoanDetails(Long loanId) {
         return loanPersistencePort.findLoanDetails(loanId, authenticationPort.getAuthenticatedUserId());
+    }
+
+    @Override
+    public void makePayment(Long loanId, Long pocketId, Double amount) {
+        Long userId = authenticationPort.getAuthenticatedUserId();
+        Loan loan = loanPersistencePort.findLoanDetails(loanId, userId);
+
+        if (loan == null) {
+            throw new ResourceNotFoundException(DomainConstants.LOAN_NOT_FOUND_MESSAGE);
+        }
+
+        if (loan.getLoanType().equals(LoanType.BORROWER)) {
+            if (loan.getBalance() < amount) {
+                throw new InsufficientBalanceException(DomainConstants.INSUFFICIENT_BALANCE_MESSAGE);
+            }
+        }
+
+        String categoryName = loan.getLoanType() == LoanType.LENDER ? "Cobrar" : "Reembolsar";
+        Pocket pocket = pocketUseCase.getPocketByIdAndUserId(pocketId);
+        Category category = categoryUseCase.getCategoryByName(categoryName);
+        Long existingPocketId = pocket.getId();
+
+        if (loan.getLoanType().equals(LoanType.BORROWER)) {
+            if (!pocketUseCase.isPocketBalanceSufficient(existingPocketId, amount)) {
+                throw new InsufficientBalanceException(DomainConstants.INSUFFICIENT_BALANCE_MESSAGE);
+            }
+        }
+
+        loan.setBalance(loan.getBalance() - amount);
+
+        if (loan.getBalance() <= 0) {
+            loan.setLoanStatus(LoanStatus.COMPLETED);
+        }
+
+        loanPersistencePort.saveLoan(loan);
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setDescription(loan.getLoanType() == LoanType.LENDER ? "Cobro" : "Reembolso");
+        transaction.setTransactionType(loan.getLoanType() == LoanType.LENDER ? TransactionType.INCOME : TransactionType.EXPENSE);
+        transaction.setDate(LocalDateTime.now());
+        transaction.setPeriodicity(Periodicity.ONCE);
+        transaction.setCategoryId(category.getId());
+        transaction.setLoanId(loan.getId());
+        transaction.setPocketId(pocketId);
+
+        transactionUseCase.saveNewTransaction(transaction);
     }
 }
