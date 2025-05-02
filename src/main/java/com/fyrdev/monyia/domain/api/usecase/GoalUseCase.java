17ba20a2758beac6d1cae316cdad9d11b29b1e9e
Fabian Yate <fabianyate31@gmail.com>
2025-05-02 00:13:08 -0500

    Add GoalUseCase for goal management and update DomainConstants with new messages

---
+++ src/main/java/com/fyrdev/monyia/domain/api/usecase/GoalUseCase.java
@@ -0,0 +1,130 @@
+package com.fyrdev.monyia.domain.api.usecase;
+
+import com.fyrdev.monyia.configuration.exceptionhandler.ResourceNotFoundException;
+import com.fyrdev.monyia.domain.api.ICategoryServicePort;
+import com.fyrdev.monyia.domain.api.IGoalServicePort;
+import com.fyrdev.monyia.domain.api.ITransactionServicePort;
+import com.fyrdev.monyia.domain.exception.InsufficientBalanceException;
+import com.fyrdev.monyia.domain.model.Category;
+import com.fyrdev.monyia.domain.model.Goal;
+import com.fyrdev.monyia.domain.model.Transaction;
+import com.fyrdev.monyia.domain.model.enums.GoalTransactionType;
+import com.fyrdev.monyia.domain.model.enums.Periodicity;
+import com.fyrdev.monyia.domain.model.enums.TransactionType;
+import com.fyrdev.monyia.domain.spi.AITextClassifierPort;
+import com.fyrdev.monyia.domain.spi.IAuthenticationPort;
+import com.fyrdev.monyia.domain.spi.IGoalPersistencePort;
+import com.fyrdev.monyia.domain.util.DomainConstants;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GoalUseCase implements IGoalServicePort {
+    private final IGoalPersistencePort goalPersistencePort;
+    private final IAuthenticationPort authenticationPort;
+    private final AITextClassifierPort aiTextClassifierPort;
+    private final ITransactionServicePort transactionServicePort;
+    private final ICategoryServicePort categoryServicePort;
+
+    public GoalUseCase(IGoalPersistencePort goalPersistencePort,
+                       IAuthenticationPort authenticationPort,
+                       AITextClassifierPort aiTextClassifierPort,
+                       ITransactionServicePort transactionServicePort,
+                       ICategoryServicePort categoryServicePort) {
+        this.goalPersistencePort = goalPersistencePort;
+        this.authenticationPort = authenticationPort;
+        this.aiTextClassifierPort = aiTextClassifierPort;
+        this.transactionServicePort = transactionServicePort;
+        this.categoryServicePort = categoryServicePort;
+    }
+
+
+    @Override
+    public Goal createNewGoal(Goal goal) {
+        Long userId = authenticationPort.getAuthenticatedUserId();
+        String emoji = aiTextClassifierPort.suggestEmojis(goal.getName()).get(0);
+
+        goal.setUserId(userId);
+        goal.setEmoji(emoji);
+        goal.setBalance(goal.getAmount());
+
+        return goalPersistencePort.createNewGoal(goal);
+    }
+
+    @Override
+    public List<Goal> getAllGoalsByUserId() {
+        Long userId = authenticationPort.getAuthenticatedUserId();
+
+        return goalPersistencePort.getAllGoalsByUserId(userId);
+    }
+
+    @Override
+    public Goal getGoalById(Long goalId) {
+        Long userId = authenticationPort.getAuthenticatedUserId();
+
+        if (!goalPersistencePort.goalExists(goalId, userId)) {
+            throw new ResourceNotFoundException((DomainConstants.GOAL_NOT_FOUND));
+        }
+
+        return goalPersistencePort.getGoalById(goalId, userId);
+    }
+
+    @Override
+    public void makeDepositOrWithdraw(Long goalId, Double amount, GoalTransactionType type) {
+        Long userId = authenticationPort.getAuthenticatedUserId();
+        Goal goal = getGoalById(goalId);
+        String categoryName = setCategoryName(type);
+        Category category = categoryServicePort.getCategoryByName(categoryName);
+
+        if (amount <= 0) {
+            throw new InsufficientBalanceException(DomainConstants.INVALID_DEPOSIT_AMOUNT);
+        }
+
+        if (goal.getBalance() < amount) {
+            throw new InsufficientBalanceException(DomainConstants.INSUFFICIENT_BALANCE_MESSAGE);
+        }
+
+        goal.setBalance(updateBalance(type, amount, goal.getBalance()));
+        goal.setUserId(userId);
+
+        goalPersistencePort.createNewGoal(goal);
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setDescription(setDescription(type, goal.getName()));
+        transaction.setTransactionType(setTransactionType(type));
+        transaction.setDate(LocalDateTime.now());
+        transaction.setPeriodicity(Periodicity.ONCE);
+        transaction.setCategoryId(category.getId());
+        transaction.setGoalId(goal.getId());
+        transaction.setPocketId(null);
+
+        transactionServicePort.saveNewTransaction(transaction);
+
+    }
+
+    Double updateBalance(GoalTransactionType type, Double amount, Double currentBalance) {
+        return type.equals(GoalTransactionType.DEPOSIT) ?
+                currentBalance - amount :
+                currentBalance + amount;
+    }
+
+    TransactionType setTransactionType(GoalTransactionType type) {
+        return type.equals(GoalTransactionType.DEPOSIT) ?
+                TransactionType.INCOME :
+                TransactionType.EXPENSE;
+    }
+
+    String setCategoryName(GoalTransactionType type) {
+        return type.equals(GoalTransactionType.DEPOSIT) ?
+                "Depósito" :
+                "Retiro";
+    }
+
+    String setDescription(GoalTransactionType type, String goalName) {
+        return type.equals(GoalTransactionType.DEPOSIT) ?
+                "Depósito a meta " + goalName :
+                "Retiro de meta" + goalName;
+    }
+
+}
--- src/main/java/com/fyrdev/monyia/domain/util/DomainConstants.java
+++ src/main/java/com/fyrdev/monyia/domain/util/DomainConstants.java
@@ -11,7 +11,9 @@ public class DomainConstants {
     public static final String EMAIL_ALREADY_EXISTS_MESSAGE = "El correo ya existe";
     public static final String INSUFFICIENT_BALANCE_MESSAGE = "Saldo insuficiente";
     public static final String USER_SUCCESSFULLY_REGISTERED_MESSAGE = "Registro exitoso. Por favor, inicie sesión";
+    public static final String INVALID_DEPOSIT_AMOUNT = "EL monto a depositar debe ser mayor a 0";
     public static final String USER_SUCCESSFULLY_LOGGED_MESSAGE = "Sesión exitosa";
+    public static final String GOAL_NOT_FOUND = "Meta no encontrada";
     public static final String USER_ID = "userId";
     public static final String LOAN_NOT_FOUND_MESSAGE = "Deuda no encontrada";
     public static final String EMAIL_NOT_FOUND_MESSAGE = "Email not found";
